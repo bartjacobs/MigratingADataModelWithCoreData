821f730a6770aee3fc364ecbe52f2602f6039bea
Bart Jacobs <developer@bartjacobs.com>
2017-03-07 08:39:01 +0100

    Set up Core Data stack in CoreDataManager

--- Lists/Managers/CoreDataManager.swift
+++ Lists/Managers/CoreDataManager.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2017 Cocoacasts. All rights reserved.
 //
 
-import Foundation
+import CoreData
+
+final class CoreDataManager {
+
+    // MARK: - Properties
+
+    private let modelName: String
+
+    // MARK: - Initialization
+
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+
+    // MARK: - Core Data Stack
+
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+
+        return managedObjectContext
+    }()
+
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to Find Data Model")
+        }
+
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to Load Data Model")
+        }
+        
+        return managedObjectModel
+    }()
+
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                                              configurationName: nil,
+                                                              at: persistentStoreURL,
+                                                              options: nil)
+        } catch {
+            fatalError("Unable to Load Persistent Store")
+        }
+        
+        return persistentStoreCoordinator
+    }()
+
+}
